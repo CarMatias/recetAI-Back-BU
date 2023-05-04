26c9923ba0ecf7ab4bd5e6c23501eb6d7c4c6713
CarMatias <matiasbcar_31@hotmail.com>
2023-05-03 23:56:57 -0300

    remove g k

--- src/services/Translate/googleTranslate.ts
+++ src/services/Translate/googleTranslate.ts
@@ -2,7 +2,7 @@ import { Translate } from '@google-cloud/translate/build/src/v2'
 
 class GoogleTranslateService {
   async translate(text: string, target: 'es' | 'en'): Promise<string> {
-    const translate = new Translate({ key: 'AIzaSyD4TnYJ2h-1pi50UetORAjBICV8lYI3_14' })
+    const translate = new Translate({ key: /*keyGoogleTranslateService*/ '' })
     const [translation] = await translate.translate(text, { from: target === 'en' ? 'es' : 'en', to: target })
     return translation
   }
